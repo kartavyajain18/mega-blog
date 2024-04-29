895b3b578cec61afaf5cbedfe664b785223e262e
Kartavya Jain <kartavya2552@gmail.com>
2024-04-29 12:42:15 +0530

    first commit

---
+++ src/components/RTE.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Editor } from '@tinymce/tinymce-react';
+import { Controller } from 'react-hook-form';
+
+
+export default function RTE({ name, control, label, defaultValue = "" }) {
+    return (
+        <div className='w-full'>
+            {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
+
+            <Controller
+                name={name || "content"}
+                control={control}
+                render={({ field: { onChange } }) => (
+                    <Editor
+                        initialValue={defaultValue}
+                        apiKey='klj9lln71jqenu1ccrz0wj8xsf29yo3j8h4zckqaweyi5rva'
+                        init={{
+                            initialValue: defaultValue,
+                            height: 500,
+                            menubar: true,
+                            plugins: [
+                                "image",
+                                "advlist",
+                                "autolink",
+                                "lists",
+                                "link",
+                                "image",
+                                "charmap",
+                                "preview",
+                                "anchor",
+                                "searchreplace",
+                                "visualblocks",
+                                "code",
+                                "fullscreen",
+                                "insertdatetime",
+                                "media",
+                                "table",
+                                "code",
+                                "help",
+                                "wordcount",
+                                "anchor",
+                            ],
+                            toolbar:
+                                "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
+                            content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
+                        }}
+                        onEditorChange={onChange}
+                    />
+                )}
+            />
+
+        </div>
+    )
+}
